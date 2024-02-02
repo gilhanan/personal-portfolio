bc9f239cf0d7b818f62df5c273f2e7fad3d07e90
Gil Hanan <gil.hanan@gmail.com>
2024-02-02 23:11:23 +0200

    Fix Contact darkmode text color

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import { ContactForm } from "@contact/components/ContactForm";
 export default function ContactPage() {
   return (
     <div className="flex flex-col gap-4">
-      <p>
+      <p className="text-secondary">
         If you have any questions, comments or just want to get in touch, please feel free
         to drop me a message below. I look forward to connecting with you!
       </p>
